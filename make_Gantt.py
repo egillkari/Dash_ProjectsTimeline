46e3f72324228239e01d4d8b9493f8323d1ccf0f
Egill Kári Guðbergsson <70210296+egillkari@users.noreply.github.com>
2023-12-28 18:40:27 +0000

    Update make_Gantt.py

--- make_Gantt.py
+++ make_Gantt.py
@@ -17,7 +17,7 @@ try:
                         "Phase": "string", "PM": "string", "Tier": "string"})
    # Strip leading and trailing spaces from 'Start' and 'Finish' columns
     df['Last Updated Date'] = df['Last Updated Date'].str.strip()
-    df['Location'] = df['Location'].str.strip()    
+    df['Location'] = df['Location'].str.strip()
     df['Type'] = df['Type'].str.strip()
     df['Task'] = df['Task'].str.strip()    
     df['Phase'] = df['Phase'].str.strip()
@@ -119,25 +119,49 @@ dropdown_container_style = {
     'right': '1%',  # Adjust this value as needed to position from the right
     'top': '170px',  # Adjust top as needed to clear the header
     'zIndex': '10',  # Higher z-index to stack above the graph
-    'width': 'auto',  # Adjust width as necessary
-    #'width': '200px',
     'display': 'flex',  # Using flex to align side by side
-    'overflowY': 'visible',  # Allow the dropdown content to overflow
-    'marginBottom': '10px',
-    'marginLeft': '20px',
-    'marginRight': '10px'
+    'flexDirection': 'row',  # Align the dropdowns in a row
+    'alignItems': 'flex-start',  # Align items at the start of the container
+    'gap': '10px',  # Spacing between each dropdown
+    'paddingTight': '10px',
 }
+
 # Individual dropdown details style
 dropdown_details_style = {
     'display': 'inline-block',  # Using inline-block for side by side layout
-    'width': 'auto',  # Set to auto or specific width as per design requirement
+    'width': '150px',  # Define a width that accommodates the content
     'verticalAlign': 'top',  # To align at the top if they have different heights
-    'marginRight': '10px',  # Space between dropdowns
     'border': '1px solid #d3d3d3',
     'borderRadius': '4px',
     'padding': '5px',
-    'background-color': '#f8f9fa'
+    'background-color': '#f8f9fa',
+    'overflow': 'visible',  # Allow the dropdown content to overflow
 }
+# Left-aligned dropdown container style
+left_dropdown_container_style = {
+    'position': 'absolute',  # Position it absolutely to place it on top of other elements
+    'left': '1%',  # Adjust this value as needed to position from the left
+    'top': '150px',  # Adjust top as needed to clear the header
+    'zIndex': '5',  # Higher z-index to stack above the graph
+    'display': 'flex',  # Using flex to align side by side
+    'flexDirection': 'row',  # Align the dropdowns in a row
+    'alignItems': 'flex-start',  # Align items at the start of the container
+    'gap': '10px',  # Spacing between each dropdown
+}
+
+# Right-aligned dropdown container style
+right_dropdown_container_style = {
+    'position': 'absolute',  # Position it absolutely to place it on top of other elements
+    'right': '1%',  # Adjust this value as needed to position from the right
+    'top': '170px',  # Adjust top as needed to clear the header
+    'zIndex': '10',  # Higher z-index to stack above the graph
+    'display': 'flex',  # Using flex to align side by side
+    'flexDirection': 'row',  # Align the dropdowns in a row
+    'alignItems': 'flex-start',  # Align items at the start of the container
+    'gap': '10px',  # Spacing between each dropdown
+}
+
+
 # Sort the project list alphabetically
 project_options = [{'label': project, 'value': project} for project in sorted(df['Task'].unique())]
 
@@ -148,175 +172,155 @@ stage_options = [{'label': stage, 'value': stage} for stage in df['Phase'].uniqu
 pm_options = [{'label': pm, 'value': pm} for pm in sorted(df['PM'].unique())]
 
 
+# Header layout with title, button, and logos
+header_layout = html.Div([
+    # Title and Timeline Slider button
+    html.Div([
+        html.H1([
+            html.Span('Projects', style={"color": "#101010", "fontWeight": "bold"}),
+            html.Span('Timeline', style={"color": "#004DF4", "fontWeight": "bold"}),
+        ], style={'display': 'inline-block', 'marginRight': '20px',"fontSize": "48px",'verticalAlign': 'middle',}),
+        
+        html.Button('Timeline Slider', id='toggle-slider-button', n_clicks=0, style={
+            'backgroundColor': '#004DF4',
+            'color': 'white',
+            'border': 'none',
+            'borderRadius': '5px',
+            'padding': '10px 20px',
+            'cursor': 'pointer',
+            'fontSize': '16px',
+            'outline': 'none',
+            'display': 'inline-block',  # Display inline with the title
+            'marginRight': '20px',  # Space between the button and the logos
+        }),
+    ], style={'display': 'inline-block', 'verticalAlign': 'middle', 'width': 'auto'}),
+    
+    # Logos
+    html.Div([
+        html.Img(src="/assets/isavia_logo.png", style={
+            "height": "80px",
+            "marginRight": "20px",
+            "filter": "grayscale(100%)"
+        }),
+        html.Img(src="/assets/KEF-EN-svart.png", style={
+            "height": "80px",
+            "filter": "grayscale(100%)"
+        }),
+    ], style={'display': 'inline-block', 'verticalAlign': 'middle', 'float': 'right'}),
+    
+], style={"padding": "10px 0", "overflow": "auto", "whiteSpace": "nowrap", "position": "relative"})
+
+# Last Updated Date positioned at the bottom right of the header
+last_updated_layout = html.Div([
+    html.P("Last Updated: " + last_updated_date_str, style={
+        'textAlign': 'right',
+        'color': '#101010',
+        'fontSize': '14px',
+        'marginTop': '5px'
+    })
+], style={'position': 'absolute', 'top': '0px', 'right': '20px'})
 
 #isavia blue : #4396a7 orange:#e65500
 # App layout
 app.layout = html.Div(style={'backgroundColor': 'white', 'color': '#101010', 'fontFamily': 'Arial'}, children=[
+    header_layout,
+    last_updated_layout,
     
-    # Header
-    html.Div([
-        html.Div([
-            html.H1([
-                html.Span('Projects', style={"color": "#101010", "fontWeight": "bold"}),
-                html.Span('Timeline', style={"color": "#004DF4", "fontWeight": "bold"})
-            ], style={
-                "marginLeft": "20px",  # Ensure some space on the left
-                "fontSize": "48px",  # Maintain large font size for visibility
-                "padding": "10px"  # Add some padding around the text
-            }),
-        ], style={'display': 'inline-block', 'width': '50%', 'verticalAlign': 'middle'}),
-        
-        # Logo container
-        html.Div([
-            html.Img(src="/assets/isavia_logo.png",
-                    style={
-                        "height": "80px",  # Adjusted height for the logo
-                        "marginRight": "30px",  # Space between logos
-                        "verticalAlign": "middle",  # Aligns the image vertically in the middle
-                        "filter": "grayscale(100%)"  # Converts the image to black and white
-                    }),
-            html.Img(src="/assets/KEF-EN-svart.png",
-                    style={
-                        "height": "80px",  # Adjusted height for the logo
-                        "verticalAlign": "middle",  # Aligns the image vertically in the middle
-                        "filter": "grayscale(100%)"  # Converts the image to black and white
-                    }),
-        ], style={'display': 'inline-block', 'width': '50%', 'textAlign': 'right'}),
-
-        
-    ], style={"textAlign": "left", "padding": "20px 0", "lineHeight": "30px"}),  # lineHeight helps vertically center the text and logos
-
-    # New Div for Last Updated Date
-    html.Div([
-        html.P("Last Updated: " + last_updated_date_str, style={
-            'textAlign': 'right',
-            'color': '#101010',
-            'fontSize': '14px',
-            'marginRight': '20px',
-            'marginTop': '20px'
-        })
-    ], style={'position': 'absolute', 'top': 0, 'right': 0}),  # Adjust positioning as needed
-
     # Filters and sorting options
-    html.Div(style=filter_sort_container_style, children=[
-        # Filter by PM/Phase
-        html.Div([
-            html.Label('Color by:', style={'paddingRight': '10px'}),
-            dcc.RadioItems(
-                options=[
-                    {'label': 'Stages', 'value': 'Phase'},
-                    {'label': 'PM', 'value': 'PM'}
-                ],
-                value='Phase',
-                id='color-radio-items',
-                inline=True,
-                style={"color": "black"}
-            ),
-        ], style=filter_container_style),
-
-        # Filter Data by Location
-        html.Div([
-            html.Label('Location:', style={'paddingRight': '10px'}),
-            dcc.Checklist(
-                options=[
-                    {'label': 'Terminals', 'value': 'Terminals'},
-                    {'label': 'Airfield', 'value': 'Airfield'},
-                    {'label': 'Landside', 'value': 'Landside'}
-                ],
-                value=['Terminals', 'Airfield'],  # All options selected by default
-                id='location-checklist-items',
-                style={"color": "black"}
-            ),
-        ], style=filter_container_style),
-
-        # Filter Data by Type
-        html.Div([
-            html.Label('Type:', style={'paddingRight': '10px'}),
-            dcc.Checklist(
-                options=[
-                    {'label': 'Building', 'value': 'Building'},
-                    {'label': 'Civil', 'value': 'Civil'},
-                    {'label': 'Utilities', 'value': 'Utilities'},
-                    {'label': 'Strategies and Plans', 'value': 'Strategies and Plans'},
-                    {'label': 'MOI', 'value': 'MOI'}
-                ],
-                value=['Building', 'Civil'],  # All options selected by default
-                id='type-checklist-items',
-                style={"color": "black"}
-            ),
-        ], style=filter_container_style),
-
-        # Filter Data by Tier
-        html.Div([
-            html.Label('Filter by Tier:', style={'paddingRight': '10px'}),
-            dcc.Checklist(
-                options=[
-                    {'label': 'Tier 1', 'value': '1'},
-                    {'label': 'Tier 2', 'value': '2'},
-                    {'label': 'Tier 3', 'value': '3'}
-                ],
-                value=['1', '2', '3'],  # All options selected by default
-                id='tier-checklist-items',
-                style={"color": "black"}
-            ),
-        ], style=filter_container_style),
-        
-        # Dropdown for sorting options
-        html.Div([
-            html.Label('Sort Projects By:', style={'paddingRight': '10px'}),
-            dcc.Dropdown(
-                id='sort-dropdown',
-                options=[
-                    {'label': 'Project Start', 'value': 'Project_Start'},
-                    {'label': 'Project Finish', 'value': 'Project_Finish'},
-                    {'label': 'Start of Stage 3', 'value': 'Stage_3_Start'},  
-                    {'label': 'Start of Procurement', 'value': 'Procurement_Start'},  
-                    {'label': 'Start of Construction', 'value': 'Stage_5_Start'},
-                    {'label': 'Project Manager (PM)', 'value': 'PM'},
-                    {'label': 'Alphabetically', 'value': 'Task'},  # Added this line
-
-                    # ... (add other stages as needed)
-                ],
-                value='Project_Start',
-                clearable=False,
-                style={"width": "100%"}
-            ),
-        ], style=filter_container_style),
-
-        # Button for toggling range slider
-        html.Div([
-            html.Button('Timeline Slider', id='toggle-slider-button', n_clicks=0, style={
-                'backgroundColor': '#004DF4',
-                'color': 'white',
-                'border': 'none',
-                'borderRadius': '5px',
-                'padding': '10px 20px',
-                'cursor': 'pointer',
-                'fontSize': '16px',
-                'outline': 'none',
-                'marginRight': '10px',  # Add space to the right of the button
-            })
+    html.Div(style={'position': 'absolute', 'top': '130px', 'left': '1%', 'right': '1%', 'zIndex': '10'}, children=[
+        # Left-aligned options container
+        html.Div(style={'display': 'flex', 'flexWrap': 'nowrap', 'justifyContent': 'start', 'alignItems': 'flex-start', 'marginRight': '50%'}, children=[
+            # Filter by PM/Phase
+            html.Div(style=filter_container_style, children=[
+                html.Label('Color by:', style={'paddingRight': '10px'}),
+                dcc.RadioItems(
+                    options=[
+                        {'label': 'Stages', 'value': 'Phase'},
+                        {'label': 'PM', 'value': 'PM'}
+                    ],
+                    value='Phase',
+                    id='color-radio-items',
+                    inline=True,
+                    style={"color": "black"}
+                ),
+            ]),
             
-        ], style=filter_container_style),
+            # Filter Data by Location
+            html.Div(style=filter_container_style, children=[
+                html.Label('Location:', style={'paddingRight': '10px'}),
+                dcc.Checklist(
+                    options=[
+                        {'label': 'Terminals', 'value': 'Terminals'},
+                        {'label': 'Airfield', 'value': 'Airfield'},
+                        {'label': 'Landside', 'value': 'Landside'}
+                    ],
+                    value=['Terminals', 'Airfield'],
+                    id='location-checklist-items',
+                    style={"color": "black"}
+                ),
+            ]),
 
+            # Filter Data by Type
+            html.Div(style=filter_container_style, children=[
+                html.Label('Type:', style={'paddingRight': '10px'}),
+                dcc.Checklist(
+                    options=[
+                        {'label': 'Building', 'value': 'Building'},
+                        {'label': 'Civil', 'value': 'Civil'},
+                        {'label': 'Utilities', 'value': 'Utilities'},
+                        {'label': 'Strategies and Plans', 'value': 'Strategies and Plans'},
+                        {'label': 'MOI', 'value': 'MOI'}
+                    ],
+                    value=['Building', 'Civil'],
+                    id='type-checklist-items',
+                    style={"color": "black"}
+                ),
+            ]),
 
-        # An invisible container that still takes up space
-        html.Div(style={
-            #'display': 'flex',
-            'flexDirection': 'column',
-            'justifyContent': 'flex-start',  # Align items to the top
-            'paddingRight': '10px',
-            'height': '150px', 
-            'width': '400',
-            'visibility': 'hidden',
-            'minWidth': '400px'
-            }),
+            # Filter Data by Tier
+            html.Div(style=filter_container_style, children=[
+                html.Label('Filter by Tier:', style={'paddingRight': '10px'}),
+                dcc.Checklist(
+                    options=[
+                        {'label': 'Tier 1', 'value': '1'},
+                        {'label': 'Tier 2', 'value': '2'},
+                        {'label': 'Tier 3', 'value': '3'}
+                    ],
+                    value=['1', '2', '3'],
+                    id='tier-checklist-items',
+                    style={"color": "black"}
+                ),
+            ]),
+            # Sorting dropdown
+            html.Div(style={
+                    'display': 'flex',
+                    'flexDirection': 'column',
+                    'justifyContent': 'flex-start',  # Align items to the top
+                    'paddingRight': '10px',
+                    'minWidth': '170px',
+                    #'maxWidth': '120px'
+                }, children=[
+                html.Label('Sort Projects By:', style={'paddingRight': '10px'}),
+                dcc.Dropdown(
+                    id='sort-dropdown',
+                    options=[
+                        {'label': 'Project Start', 'value': 'Project_Start'},
+                        {'label': 'Project Finish', 'value': 'Project_Finish'},
+                        {'label': 'Start of Stage 3', 'value': 'Stage_3_Start'},  
+                        {'label': 'Start of Procurement', 'value': 'Procurement_Start'},  
+                        {'label': 'Start of Construction', 'value': 'Stage_5_Start'},
+                        {'label': 'Project Manager (PM)', 'value': 'PM'},
+                        {'label': 'Alphabetically', 'value': 'Task'},
+                    ],
+                    value='Project_Start',
+                    clearable=False,
+                    style={"width": "100%"}
+                ),
+            ]),
+        ]),
 
-
-
-        # Dropdown container
-        html.Div(style=dropdown_container_style, children=[
-            # Your dropdown details/checklists here
+        # The dropdowns for Stages, PMs, and Projects will be aligned next to the sorting options on the right
+        html.Div(style={'paddingRight': '10px','display': 'inline-block', 'verticalAlign': 'top', 'position': 'absolute', 'right': '1%', 'top': '0','gap': '10px','alignItems': 'flex-start'}, children=[
             html.Details([
                 html.Summary('Select Stages:', style={'fontWeight': 'bold'}),
                 dcc.Checklist(
@@ -324,8 +328,9 @@ app.layout = html.Div(style={'backgroundColor': 'white', 'color': '#101010', 'fo
                     options=stage_options,
                     value=[],
                     style={"color": "black"}
-                )
+                ),
             ], style=dropdown_details_style),
+
             html.Details([
                 html.Summary('Select PMs:', style={'fontWeight': 'bold'}),
                 dcc.Checklist(
@@ -333,28 +338,30 @@ app.layout = html.Div(style={'backgroundColor': 'white', 'color': '#101010', 'fo
                     options=pm_options,
                     value=[],
                     style={"color": "black"}
-                )
+                ),
             ], style=dropdown_details_style),
-            # New dropdown for filtered projects
+
             html.Details([
                 html.Summary('Select Projects:', style={'fontWeight': 'bold'}),
                 dcc.Checklist(
                     id='filtered-project-list-checklist',
                     options=[],  # Initially empty, will be populated dynamically
                     value=[],
-                    #multi=True,  # Allows selection of multiple projects
                     style={"color": "black"}
-                )
+                ),
             ], style=dropdown_details_style),
         ]),
     ]),
+    # New Div for spacing
+    html.Div(style={'height': '50pt'}),
 
     # Graph container with lower z-index
     dcc.Graph(id='gantt-chart-placeholder', style={
         "height": "1000px",
         "backgroundColor": "#4396a7",
-        'zIndex': '1'  # Ensure this is lower than the dropdown z-index
+        'zIndex': '1'
     }),
+
 ])
 
 
